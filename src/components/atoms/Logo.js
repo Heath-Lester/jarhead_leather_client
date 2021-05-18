ce2b2e59b708965cc218b2b5d117c55e905aa20f
Heath Lester <h.allen.lester@gmail.com>
2021-05-18 14:13:27 -0500

    added split nav

--- src/components/atoms/Logo.js
+++ src/components/atoms/Logo.js
@@ -2,24 +2,29 @@ import React from "react"
 import PropTypes from "prop-types"
 import "./logo.css"
 import jhlogo from "../../images/jhlogo.jpg"
+import jhlogoPlain from "../../images/jhlogoPlain.png"
 
-export const Logo = ({ size, shape, ...props }) => {
+export const Logo = ({ size, lettering, shape, ...props }) => {
+    const image = lettering ? jhlogo : jhlogoPlain
     return (
         <img
             className={`jhlogo--${size}`}
-            src={jhlogo}
+            src={image}
             alt="Jarhead Leather Co Logo" 
             {...props}
             />
     )
 }
 
+
 Logo.propTypes = {
+    lettering: PropTypes.bool,
     size: PropTypes.oneOf(['small', 'medium', 'large']),
     onClick: PropTypes.func,
 }
 
 Logo.defaultProps = {
+    lettering: true,
     size: 'medium',
     onClick: undefined,
 }
