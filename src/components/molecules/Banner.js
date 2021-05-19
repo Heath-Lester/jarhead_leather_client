673ac29e71317de5f8839c41aee32f609d59090a
Heath Lester <h.allen.lester@gmail.com>
2021-05-19 00:48:16 -0500

    finished carousel

--- src/components/molecules/Banner.js
+++ src/components/molecules/Banner.js
@@ -1,11 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Carousel from 'react-bootstrap/Carousel'
-import Coffee from "../../images/coffee.jpg"
-import BottleOpener from "../../images/bottleopener.jpg"
-import Bag from "../../images/bag.jpg"
-import Koozies from "../../images/koozies.jpg"
-import Wallets from "../../images/wallets.jpg"
+import Coffee from "../../images/carousel/coffee(2).jpg"
+import BottleOpener from "../../images/carousel/bottleopener(2).jpg"
+import Bag from "../../images/carousel/bag(2).jpg"
+import Koozies from "../../images/carousel/koozies(2).jpg"
+import Wallets from "../../images/carousel/wallets(2).jpg"
 import "./banner.css"
 
 export const Banner = ({ interval, ...props }) => {
@@ -75,5 +75,5 @@ Banner.propTypes = {
 }
 
 Banner.defaultProps = {
-    interval: 1000,
+    interval: 7000,
 }
